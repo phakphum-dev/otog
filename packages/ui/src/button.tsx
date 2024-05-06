e06f6d186bb17e16fb2506afb14c2fc07f5eae8d
Anon-136 <anonjin44@gmail.com>
2024-05-06 13:51:16 +0700

    :bug: fix icon in a button

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -6,7 +6,7 @@ import { type VariantProps, cva } from 'class-variance-authority'
 import { cn } from './utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center gap-2 justify-center whitespace-nowrap rounded-md text-sm font-semibold transition-colors ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center gap-2 justify-center whitespace-nowrap rounded-md text-sm font-semibold transition-colors ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  [&>svg]:size-4',
   {
     variants: {
       variant: {
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10 [&>svg]:size-4',
+        icon: 'h-10 w-10',
       },
     },
     defaultVariants: {
