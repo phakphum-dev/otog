ca1bbc5973e6178cf920fea2fc9a86c992f74864
Anon-136 <anonjin44@gmail.com>
2025-01-25 18:32:10 +0700

    :recycle: move table pagination to share table-component

--- apps/web/src/pages/admin/index.tsx
+++ apps/web/src/pages/admin/index.tsx
@@ -1,4 +1,4 @@
-import { useId, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 
@@ -18,20 +18,12 @@ import { useReactTable } from '@tanstack/react-table'
 import {
   ColumnFiltersState,
   Row,
-  Table as TanstackTable,
   VisibilityState,
   createColumnHelper,
   getCoreRowModel,
 } from '@tanstack/table-core'
 import { File } from '@web-std/file'
-import {
-  ChevronFirst,
-  ChevronLast,
-  ChevronLeft,
-  ChevronRight,
-  PencilIcon,
-  Search,
-} from 'lucide-react'
+import { PencilIcon } from 'lucide-react'
 import NextLink from 'next/link'
 import { z } from 'zod'
 
@@ -68,29 +60,19 @@ import {
   FormLabel,
   FormMessage,
 } from '@otog/ui/form'
-import { Input, InputGroup, InputLeftIcon } from '@otog/ui/input'
-import { Label } from '@otog/ui/label'
+import { Input } from '@otog/ui/input'
 import { Link } from '@otog/ui/link'
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-} from '@otog/ui/pagination'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@otog/ui/select'
-import { Spinner } from '@otog/ui/spinner'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@otog/ui/tabs'
 
 import { problemKey, problemQuery, submissionQuery } from '../../api/query'
 import { withSession } from '../../api/server'
-import { DebouncedInput } from '../../components/debounced-input'
 import { FileInput } from '../../components/file-input'
-import { TableComponent } from '../../components/table-component'
+import {
+  TableComponent,
+  TablePagination,
+  TablePaginationInfo,
+  TableSearch,
+} from '../../components/table-component'
 import { useUserContext } from '../../context/user-context'
 
 interface AdminProblemPageProps {}
@@ -206,162 +188,6 @@ function ProblemDataTable() {
         isError={adminProblems.isError}
       />
       <TablePagination table={table} isLoading={adminProblems.isFetching} />
-    </div>
-  )
-}
-
-const TableSearch = ({ table }: { table: TanstackTable<any> }) => {
-  return (
-    <InputGroup className="w-full sm:w-80">
-      <InputLeftIcon>
-        <Search />
-      </InputLeftIcon>
-      <DebouncedInput
-        onDebounce={(value) => {
-          table.resetPageIndex()
-          table.setGlobalFilter(value)
-        }}
-        placeholder="ค้นหา..."
-      />
-    </InputGroup>
-  )
-}
-
-const TablePagination = ({
-  table,
-  isLoading,
-}: {
-  table: TanstackTable<any>
-  isLoading: boolean
-}) => {
-  const id = useId()
-  return (
-    <div className="flex items-center justify-between gap-8">
-      {/* Results per page */}
-      <div className="flex items-center gap-3">
-        <Label htmlFor={id} className="max-sm:sr-only">
-          แสดง
-        </Label>
-        <Select
-          value={table.getState().pagination.pageSize.toString()}
-          onValueChange={(value) => {
-            table.setPageSize(Number(value))
-          }}
-        >
-          <SelectTrigger id={id} className="w-fit whitespace-nowrap">
-            <SelectValue placeholder="Select number of results" />
-          </SelectTrigger>
-          <SelectContent className="[&_*[role=option]>span]:end-2 [&_*[role=option]>span]:start-auto [&_*[role=option]]:pe-8 [&_*[role=option]]:ps-2">
-            {[5, 10, 25, 50].map((pageSize) => (
-              <SelectItem key={pageSize} value={pageSize.toString()}>
-                {pageSize}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
-      <TablePaginationInfo table={table} isLoading={isLoading} />
-    </div>
-  )
-}
-
-const TablePaginationInfo = ({
-  table,
-  isLoading,
-}: {
-  table: TanstackTable<any>
-  isLoading: boolean
-}) => {
-  return (
-    <div className="flex items-center justify-between gap-8">
-      {/* Page number information */}
-      <div className="flex grow justify-end whitespace-nowrap text-sm text-muted-foreground items-center gap-2">
-        {isLoading && <Spinner size="sm" />}
-        <p
-          className="whitespace-nowrap text-sm text-muted-foreground"
-          aria-live="polite"
-        >
-          <span className="text-foreground">
-            {table.getState().pagination.pageIndex *
-              table.getState().pagination.pageSize +
-              1}
-            -
-            {Math.min(
-              Math.max(
-                table.getState().pagination.pageIndex *
-                  table.getState().pagination.pageSize +
-                  table.getState().pagination.pageSize,
-                0
-              ),
-              table.getRowCount()
-            )}
-          </span>{' '}
-          จาก{' '}
-          <span className="text-foreground">
-            {table.getRowCount().toString()}
-          </span>
-        </p>
-      </div>
-
-      {/* Pagination buttons */}
-      <div>
-        <Pagination>
-          <PaginationContent>
-            {/* First page button */}
-            <PaginationItem>
-              <Button
-                size="icon"
-                variant="outline"
-                className="disabled:pointer-events-none disabled:opacity-50"
-                onClick={() => table.firstPage()}
-                disabled={!table.getCanPreviousPage()}
-                aria-label="Go to first page"
-              >
-                <ChevronFirst size={16} strokeWidth={2} aria-hidden="true" />
-              </Button>
-            </PaginationItem>
-            {/* Previous page button */}
-            <PaginationItem>
-              <Button
-                size="icon"
-                variant="outline"
-                className="disabled:pointer-events-none disabled:opacity-50"
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-                aria-label="Go to previous page"
-              >
-                <ChevronLeft size={16} strokeWidth={2} aria-hidden="true" />
-              </Button>
-            </PaginationItem>
-            {/* Next page button */}
-            <PaginationItem>
-              <Button
-                size="icon"
-                variant="outline"
-                className="disabled:pointer-events-none disabled:opacity-50"
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-                aria-label="Go to next page"
-              >
-                <ChevronRight size={16} strokeWidth={2} aria-hidden="true" />
-              </Button>
-            </PaginationItem>
-            {/* Last page button */}
-            <PaginationItem>
-              <Button
-                size="icon"
-                variant="outline"
-                className="disabled:pointer-events-none disabled:opacity-50"
-                onClick={() => table.lastPage()}
-                disabled={!table.getCanNextPage()}
-                aria-label="Go to last page"
-              >
-                <ChevronLast size={16} strokeWidth={2} aria-hidden="true" />
-              </Button>
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
-      </div>
     </div>
   )
 }
